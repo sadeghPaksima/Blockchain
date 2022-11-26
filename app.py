1032c89e2c0586ceddc0b2335122a2715e404bf3
Your Name <you@example.com>
2022-11-26 06:34:13 -0500

    create first page and private key ui

---
+++ app.py
@@ -0,0 +1,28 @@
+import dash_bootstrap_components as dbc
+import dash_daq as daq
+from dash import html,Input,Output,State,dcc,Dash
+from UI import menu,Style,PrivateKey
+from urllib.parse import unquote
+
+app=Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout=html.Div([dcc.Location(id="url"),dbc.Row([dbc.Col(menu.Create_Card())],id="Main"),dbc.Row(children=[],id="item")],id="root",className="container-fluid",style=Style.MarginTop)
+
+@app.callback(
+    Output(component_id="Main",component_property= 'style'),
+    Output(component_id="item",component_property= 'children'),
+    Input(component_id="url",component_property="pathname"),
+)
+def render(pathname):
+    if pathname=='/':
+        return Style.DisplayBlock,[]
+    else:
+        url=unquote(pathname)[1:]
+        if url in menu.menu_list:
+            functions={"Create Private key":PrivateKey.create_private_item}
+            d=functions[url]()
+            return Style.DisplayNone,d
+        
+
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0',debug=True,port=9999)
